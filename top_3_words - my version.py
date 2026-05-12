24337f8cbf136720b0dac6e3ed41f2ca53a9ff59
Dharashri <59225589+Dharashri@users.noreply.github.com>
2026-05-13 00:07:51 +0530

    Add files via upload

---
+++ top_3_words - my version.py
@@ -0,0 +1,32 @@
+def top3(text):
+    sentence=""
+    for char in text:
+        if char.isalpha() or char == " ":
+            sentence+= char.lower()
+    words = sentence.split(" ")
+    word_list = []
+    count_list = []
+
+    for word in words:
+        if word=="":
+            continue
+        if word in word_list:
+            index=word_list.index(word)
+            count_list[index] +=1
+        else:
+            word_list.append(word)
+            count_list.append(1)
+    
+    for i in range(len(word_list)-1):
+        for j in range(len(word_list)-i-1):
+            if count_list[j] < count_list[j+1]:
+                count_list[j],count_list[j+1]=count_list[j+1],count_list[j]
+                word_list[j],word_list[j+1]=word_list[j+1],word_list[j]
+
+    output = []
+    for i in range(min(3, len(word_list))):
+        output.append(word_list[i])
+    return output
+
+text = input()
+print (top3(text))
